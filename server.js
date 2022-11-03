47f27629345031a9b2c7f4ccd0dfc22b26351513
GabrielSouza2 <112514408+GabrielSouza2@users.noreply.github.com>
2022-11-03 13:03:08 -0300

    Atualizações

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const jsonServer = require('Json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+import { create, router as _router, defaults } from 'Json-server';
+const server = create();
+const router = _router('db.json');
+const middlewares = defaults();
 
 const port = process.env.PORT || 8080;
 
